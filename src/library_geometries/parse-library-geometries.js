03e73d56fd3fec20fb224f8a816c94a50e595c11
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-01 08:32:52 -0400

    Split out library geometries

---
+++ src/library_geometries/parse-library-geometries.js
@@ -0,0 +1,40 @@
+module.exports = ParseLibraryGeometries
+
+function ParseLibraryGeometries (library_geometries) {
+  var geometryMesh = library_geometries[0].geometry[0].mesh[0]
+  var source = geometryMesh.source
+
+  /* Vertex Positions, UVs, Normals */
+  var polylistIndices = geometryMesh.polylist[0].p[0].split(' ')
+
+  var vertexNormalIndices = []
+  var vertexPositionIndices = []
+  var vertexUVIndices = []
+  polylistIndices.forEach(function (vertexIndex, positionInArray) {
+    if (positionInArray % source.length === 0) {
+      vertexPositionIndices.push(Number(vertexIndex))
+    } else if (positionInArray % source.length === 1) {
+      vertexNormalIndices.push(Number(vertexIndex))
+    }
+    if (source.length > 2 && positionInArray % source.length === 2) {
+      vertexUVIndices.push(Number(vertexIndex))
+    }
+  })
+  var vertexPositions = source[0].float_array[0]._.split(' ').map(Number)
+  var vertexNormals = source[1].float_array[0]._.split(' ').map(Number)
+  var vertexUVs = []
+  // TODO: use input, semantics, source, offset, etc
+  if (source[2]) {
+    vertexUVs = source[2].float_array[0]._.split(' ').map(Number)
+  }
+  /* End Vertex Positions, UVs, Normals */
+
+  return {
+    vertexPositions: vertexPositions,
+    vertexNormals: vertexNormals,
+    vertexUVs: vertexUVs,
+    vertexNormalIndices: vertexNormalIndices,
+    vertexPositionIndices: vertexPositionIndices,
+    vertexUVIndices: vertexUVIndices
+  }
+}
