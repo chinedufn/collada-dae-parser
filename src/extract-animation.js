b9745cfc55c250f75d343c79f06053aded6ec345
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-08 10:02:42 -0400

    Add expected skinned cubes geometry
    Need to add animation keyframes / matrix data next

--- src/extract-animation.js
+++ src/extract-animation.js
@@ -12,31 +12,33 @@ function ExtractAnimation (libraryAnimations) {
   }
   var numAnims = Object.keys(libraryAnimations).length
   for (var i = 0; i < numAnims; i++) {
-    var animationSource = libraryAnimations[i].source
+    if (libraryAnimations[i].$.id.indexOf('location') !== -1 || libraryAnimations[i].$.id.indexOf('rotation') !== -1 || libraryAnimations[i].$.id.indexOf('scale') !== -1) {
+      var animationSource = libraryAnimations[i].source
 
-    var currentKeyframes = animationSource[0].float_array[0]._.split(' ').map(Number)
-    var positions = animationSource[1].float_array[0]._.split(' ').map(Number)
+      var currentKeyframes = animationSource[0].float_array[0]._.split(' ').map(Number)
+      var positions = animationSource[1].float_array[0]._.split(' ').map(Number)
 
-    // TODO: Interpolation, intangent, outtangent? Or just expect linear?
-    // Depends how much of collada spec we want to support
+      // TODO: Interpolation, intangent, outtangent? Or just expect linear?
+      // Depends how much of collada spec we want to support
 
-    var xyz = xyzMap[i % 3]
+      var xyz = xyzMap[i % 3]
 
-    Object.keys(currentKeyframes).forEach(function (aKeyframe, index) {
-      var dimension
-      if (i < 3) {
-        dimension = 'location'
-      } else if (i < 6) {
-        dimension = 'rotation'
-      } else {
-        dimension = 'scale'
-      }
+      Object.keys(currentKeyframes).forEach(function (aKeyframe, index) {
+        var dimension
+        if (i < 3) {
+          dimension = 'location'
+        } else if (i < 6) {
+          dimension = 'rotation'
+        } else {
+          dimension = 'scale'
+        }
 
-      allKeyframes[currentKeyframes[index]] = allKeyframes[currentKeyframes[index]] || {}
-      allKeyframes[currentKeyframes[index]][dimension] = allKeyframes[currentKeyframes[index]][dimension] || {}
-      allKeyframes[currentKeyframes[index]][dimension][xyz] = positions[index]
-      // foo[keys[index]] = 'shut'
-    })
+        allKeyframes[currentKeyframes[index]] = allKeyframes[currentKeyframes[index]] || {}
+        allKeyframes[currentKeyframes[index]][dimension] = allKeyframes[currentKeyframes[index]][dimension] || {}
+        allKeyframes[currentKeyframes[index]][dimension][xyz] = positions[index]
+        // foo[keys[index]] = 'shut'
+      })
+    }
   }
 
   return allKeyframes
