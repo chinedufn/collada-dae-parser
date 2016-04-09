5f615c416b98af470ecbc908c0c1e826fbb49024
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-08 22:19:38 -0400

    Parse joint keyframes

--- src/library_animations/parse-library-animations.js
+++ src/library_animations/parse-library-animations.js
@@ -1,17 +1,22 @@
 module.exports = ParseLibraryAnimations
 
-// TODO: parse interpolation. Don't hard code attribute location
+// TODO: parse interpolation
+// TODO: Don't hard code attribute location
+// TODO: Don't assume that joint animations are in order
 function ParseLibraryAnimations (library_animations) {
   var animations = library_animations[0].animation
+  var allKeyframes = {}
   animations.forEach(function (anim, index) {
     if (anim.$.id.indexOf('pose_matrix') !== -1) {
-      console.log(anim.$.id)
       var currentKeyframes = anim.source[0].float_array[0]._.split(' ').map(Number)
-      console.log(currentKeyframes)
+
       var currentJointPoseMatrices = anim.source[1].float_array[0]._.split(' ').map(Number)
       currentKeyframes.forEach(function (_, index) {
-        console.log(currentJointPoseMatrices.slice(16 * index, 16 * index + 16))
+        allKeyframes[currentKeyframes[index]] = allKeyframes[currentKeyframes[index]] || []
+        allKeyframes[currentKeyframes[index]].push(currentJointPoseMatrices.slice(16 * index, 16 * index + 16))
       })
     }
   })
+
+  return allKeyframes
 }
