bb742eb75f01d396bc732506d94a8ba7145860b2
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-08 20:14:50 -0400

    Parse vertexJointWeights into parsed collada

--- src/library_controllers/parse-library-controllers.js
+++ src/library_controllers/parse-library-controllers.js
@@ -4,34 +4,32 @@ module.exports = ParseLibraryControllers
 function ParseLibraryControllers (library_controllers) {
   var controller = library_controllers[0].controller
   if (controller) {
-    // Joints
+    // All of our models joints
     var joints = controller[0].skin[0].source[0].Name_array[0]._.split(' ')
-    console.log(joints)
 
     // Number of vertexes that need weights
-    var numVertices = controller[0].skin[0].vertex_weights[0].$.count
-    console.log(numVertices)
+    // var numVertices = controller[0].skin[0].vertex_weights[0].$.count
 
     // # of (joint,weight) pairs to read for each vertex
     // TODO: had to trim this.. should I trim everywhere?
     var jointWeightCounts = controller[0].skin[0].vertex_weights[0].vcount[0].trim().split(' ').map(Number)
 
+    // An array of all possible weights (I think?)
+    var weightsArray = controller[0].skin[0].source[2].float_array[0]._.split(' ').map(Number)
+
     // Every (joint,weight). Use jointWeightCounts to know how many to read per vertex
-    var parsedVertexJointWeights = []
+    var parsedVertexJointWeights = {}
     var jointsAndWeights = controller[0].skin[0].vertex_weights[0].v[0].split(' ').map(Number)
     jointWeightCounts.forEach(function (_, index) {
       var numJointWeightsToRead = jointWeightCounts[index]
+      parsedVertexJointWeights[index] = []
       for (var i = 0; i < numJointWeightsToRead; i++) {
-        parsedVertexJointWeights[index] = {}
         // TODO: Should we index by the actual index number or the joint name?
-        parsedVertexJointWeights[index][joints[jointsAndWeights.shift()]] = jointsAndWeights.shift()
+        var newJointWeightPair = {}
+        newJointWeightPair[joints[jointsAndWeights.shift()]] = weightsArray[jointsAndWeights.shift()]
+        parsedVertexJointWeights[index].push(newJointWeightPair)
       }
     })
-    console.log(parsedVertexJointWeights)
-
-    // An array of all possible weights (I think?)
-    var weightsArray = controller[0].skin[0].source[2].float_array[0]._.split(' ').map(Number)
-    console.log(weightsArray)
 
     // Joint bind poses
     var bindPoses = controller[0].skin[0].source[1].float_array[0]._.split(' ').map(Number)
@@ -42,5 +40,8 @@ function ParseLibraryControllers (library_controllers) {
     // Bind shape matrix (inverse bind matrix)
     var bindShapeMatrix = controller[0].skin[0].bind_shape_matrix[0].split(' ').map(Number)
     console.log(bindShapeMatrix)
+  }
+  return {
+    vertexJointWeights: parsedVertexJointWeights
   }
 }
