f8c2f06cd4dd6c22d0d8daaf1c1d0d9b1f17c541
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-09 23:28:15 -0400

    Animate with joint matrices!!!
    Still missing joint weights

--- demo/textured-blender-fault-cube/shader/vertex.glsl
+++ demo/textured-blender-fault-cube/shader/vertex.glsl
@@ -1,8 +1,12 @@
 attribute vec3 aVertexPosition;
+attribute vec2 aWeight;
 
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 
+uniform mat4 boneMatrix0;
+uniform mat4 boneMatrix1;
+
 void main (void) {
-  gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+  gl_Position = uPMatrix * uMVMatrix * boneMatrix1 * vec4(aVertexPosition, 1.0);
 }
