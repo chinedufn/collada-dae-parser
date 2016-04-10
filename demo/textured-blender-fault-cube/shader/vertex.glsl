3ab4d6c79d2da91e548658b8bd75e50e4f2c6065
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-10 11:35:03 -0400

    Use the right joints in the shader
    ANIMATION !!!

--- demo/textured-blender-fault-cube/shader/vertex.glsl
+++ demo/textured-blender-fault-cube/shader/vertex.glsl
@@ -10,6 +10,12 @@ uniform mat4 uPMatrix;
 uniform mat4 boneMatrices[2];
 
 void main (void) {
-  float foo = aJointIndex + aJointWeight;
-  gl_Position = uPMatrix * uMVMatrix * boneMatrices[1] * vec4(aVertexPosition, 1.0);
+  mat4 jointMatrix;
+  if (aJointIndex < 1.0) {
+    jointMatrix = boneMatrices[0];
+  } else if (aJointIndex < 2.0) {
+    jointMatrix = boneMatrices[1];
+  }
+  // We only have one index right now... so the weight is always 1.
+  gl_Position = uPMatrix * uMVMatrix * jointMatrix * aJointWeight * vec4(aVertexPosition, 1.0);
 }
