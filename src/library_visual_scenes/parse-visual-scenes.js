f9d349d40057c82e96fe6b354e659663a8f08648
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-14 10:04:40 -0400

    Recursively parse parent / child joints
    Still need to multiply by the parents world matrix

--- src/library_visual_scenes/parse-visual-scenes.js
+++ src/library_visual_scenes/parse-visual-scenes.js
@@ -3,23 +3,37 @@ module.exports = ParseVisualScenes
 // TODO: Handle child joints. Maybe depth first traversal?
 function ParseVisualScenes (library_visual_scenes) {
   var visualScene = library_visual_scenes[0].visual_scene[0]
-  // Check for an instance controller. If one exists we have a skeleton
-  // var joints = []
   var parsedJoints = []
+
   visualScene.node.forEach(function (node) {
+    // This is the location of all top level parent nodes
     if (node.node) {
-      node.node.forEach(function (joint) {
-        parsedJoints[joint.$.sid] = parsedJoints[joint.$.sid] || {}
-        parsedJoints[joint.$.sid].jointMatrix = joint.matrix[0]._.split(' ').map(Number)
-      })
+      // node.node is the location of all top level nodes
+      parsedJoints = parseJoints(node.node)
     }
     /*
+    // Check for an instance controller. If one exists we have a skeleton
     if (node.instance_controller) {
-      // TODO: Do I need to remove the leading `#` ?
-      joints = node.instance_controller[0].skeleton
+    // TODO: Do I need to remove the leading `#` ?
+    joints = node.instance_controller[0].skeleton
     }
     */
   })
 
   return parsedJoints
+}
+
+// Recursively parse child joints
+// TODO: factor in parent world matrix
+function parseJoints (node, accumulator) {
+  accumulator = accumulator || []
+  node.forEach(function (joint) {
+    accumulator[joint.$.sid] = accumulator[joint.$.sid] || {}
+    accumulator[joint.$.sid].jointMatrix = joint.matrix[0]._.split(' ').map(Number)
+    if (joint.node) {
+      parseJoints(joint.node, accumulator)
+    }
+  })
+
+  return accumulator
 }
