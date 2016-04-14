41d8d4b5eb4b0c3f5ea2fd127707a0e1db0d2627
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-13 22:22:36 -0400

    Add child joint test fixture
    We'll use this to test that we can properly parse and return the world
    matrices for every joint

--- test/index.js
+++ test/index.js
@@ -4,16 +4,19 @@ var path = require('path')
 var test = require('tape')
 
 var colladaFilePath = path.resolve(__dirname, './fixture/textured-blender-default-cube.dae')
-var tbdcColladaXML = fs.readFileSync(colladaFilePath).toString('utf8')
+var tbdcColladaXML = fs.readFileSync(colladaFilePath).toString()
 var expectedTBDC = require('./expected/textured-blender-default-cube.js')
 
 var blenderCubeAmimatedPath = path.resolve(__dirname, './fixture/animated-blender-cube.dae')
-var animatedColladaXML = fs.readFileSync(blenderCubeAmimatedPath).toString('utf8')
+var animatedColladaXML = fs.readFileSync(blenderCubeAmimatedPath).toString()
 var expectedAnimatedCube = require('./expected/animated-blender-cube.js')
 
 var skeletonSkinnedCubesPath = path.resolve(__dirname, './fixture/two-skinned-cubes.dae')
-var skeletonSkinnedCubesXML = fs.readFileSync(skeletonSkinnedCubesPath).toString('utf8')
+var skeletonSkinnedCubesXML = fs.readFileSync(skeletonSkinnedCubesPath).toString()
 var expectedSkinnedCube = require('./expected/two-skinned-cube.js')
+
+var threePerpendicularBonesPath = path.resolve(__dirname, './fixture/three-perpendicular-bone.dae')
+var threePerpendicularBonesXML = fs.readFileSync(threePerpendicularBonesPath).toString()
 
 var parseCollada = require('../')
 
@@ -44,6 +47,15 @@ test('Parse two blender cubes animated with bones and skinning', function (t) {
       t.fail()
     }
     t.deepEqual(parsedSkeletonSkinnedCubes, expectedSkinnedCube)
+    t.end()
+  })
+})
+
+test('Parse three bones with parent, child, child relationship', function (t) {
+  t.plan(1)
+  parseCollada(threePerpendicularBonesXML, function (err, parsedThreePerpendicularBones) {
+    if (err) { t.fail() }
+    t.ok(true)
     t.end()
   })
 })
