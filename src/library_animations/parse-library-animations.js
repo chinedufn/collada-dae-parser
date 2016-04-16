cbb4c5eae0bac3a2f7e801c2623c5b62f0c55d85
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-16 13:52:00 -0400

    Remove hard coded inv bind matrix
    Change tests and pass this in via code

--- src/library_animations/parse-library-animations.js
+++ src/library_animations/parse-library-animations.js
@@ -1,5 +1,5 @@
 var transpose = require('gl-mat4/transpose')
-var mat4Multiply = require('gl-mat4/multiply')
+// var mat4Multiply = require('gl-mat4/multiply')
 
 module.exports = ParseLibraryAnimations
 
@@ -13,11 +13,13 @@ function ParseLibraryAnimations (library_animations) {
   // TODO: This is a temporary manual insert of the two skinned cube
   // inverse bind matrices. This fixes the animation!!
   // Now we need to get this in via code
+  /*
   var count = 0
   var invBinds = [
     [1, 0, 0, -2.800068, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0.01319289, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1]
   ]
+  */
 
   animations.forEach(function (anim, index) {
     if (anim.$.id.indexOf('pose_matrix') !== -1) {
@@ -28,14 +30,11 @@ function ParseLibraryAnimations (library_animations) {
         allKeyframes[currentKeyframes[index]] = allKeyframes[currentKeyframes[index]] || []
         var currentJointMatrix = currentJointPoseMatrices.slice(16 * index, 16 * index + 16)
         // Turn our row major matrix into a column major matrix. OpenGL uses column major
-        mat4Multiply(currentJointMatrix, invBinds[count], currentJointMatrix)
+        // mat4Multiply(currentJointMatrix, invBinds[count], currentJointMatrix)
         transpose(currentJointMatrix, currentJointMatrix)
         allKeyframes[currentKeyframes[index]].push(currentJointMatrix)
       })
     }
-
-    // TODO: get rid of this. pass in matrices via code
-    count++
   })
 
   return allKeyframes
