46269cf6ef23eb136628b1f5fa802b011eae6144
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-04-17 03:05:31 -0400

    Update expected two-skinned cube keyframes with inv-bind matrix

--- src/library_animations/parse-library-animations.js
+++ src/library_animations/parse-library-animations.js
@@ -13,13 +13,6 @@ function ParseLibraryAnimations (library_animations) {
   // TODO: This is a temporary manual insert of the two skinned cube
   // inverse bind matrices. This fixes the animation!!
   // Now we need to get this in via code
-  /*
-  var count = 0
-  var invBinds = [
-    [1, 0, 0, -2.800068, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0.01319289, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1]
-  ]
-  */
 
   animations.forEach(function (anim, index) {
     if (anim.$.id.indexOf('pose_matrix') !== -1) {
@@ -29,8 +22,11 @@ function ParseLibraryAnimations (library_animations) {
       currentKeyframes.forEach(function (_, index) {
         allKeyframes[currentKeyframes[index]] = allKeyframes[currentKeyframes[index]] || []
         var currentJointMatrix = currentJointPoseMatrices.slice(16 * index, 16 * index + 16)
-        // Turn our row major matrix into a column major matrix. OpenGL uses column major
+
+        // Multiply our joint's inverse bind matrix
         // mat4Multiply(currentJointMatrix, invBinds[count], currentJointMatrix)
+
+        // Turn our row major matrix into a column major matrix. OpenGL uses column major
         transpose(currentJointMatrix, currentJointMatrix)
         allKeyframes[currentKeyframes[index]].push(currentJointMatrix)
       })
