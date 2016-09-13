29839da3c831d51b27007aebf46d9e446a4c78ff
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-09-13 19:52:38 -0400

    Pull our vertex data expansion function

---
+++ demo/animated-model/3d-model/expand-vertices.js
@@ -0,0 +1,67 @@
+module.exports = expandVertices
+
+// Takes our parsed data and expands the vertex data using our uv/vertex/normal index data
+function expandVertices (parsedDae) {
+  // Vertex normals
+  // TODO: Comment this
+  var vertexNormals = []
+
+  // TODO: naming
+  var vertexJointAffectors = []
+  var vertexJointWeights = []
+  /*
+     parsedDae.vertexJointWeights.forEach(function (jointsAndWeights) {
+     for (var i = 0; i < 4; i++) {
+     var jointIndex = Object.keys(jointsAndWeights)[i]
+     vertexJointAffectors.push(Number(jointIndex) || 0)
+     vertexJointWeights.push(jointsAndWeights[jointIndex] || 0)
+     }
+     })
+     */
+
+  // TODO: Can probably make this more efficient by indexing duplicate index -> normal -> new tail end index
+  // TODO: generally inefficient. Running calculations more than necessary. Still figuring out how to do this...
+  //  Throwing things in for now then optimize later
+  var encounteredIndices = {}
+  var vertexPositionIndices = []
+  var largestPositionIndex = 0
+  parsedDae.vertexPositionIndices.forEach(function (vertexPositionIndex, counter) {
+    largestPositionIndex = Math.max(largestPositionIndex, vertexPositionIndex)
+    if (!encounteredIndices[vertexPositionIndex]) {
+      var jointsAndWeights = parsedDae.vertexJointWeights[vertexPositionIndex]
+      // First time seeing the vertex position index
+      vertexPositionIndices[counter] = vertexPositionIndex
+      for (var i = 0; i < 4; i++) {
+        if (i < 3) {
+          vertexNormals[vertexPositionIndex * 3 + i] = parsedDae.vertexNormals[parsedDae.vertexNormalIndices[counter] * 3 + i]
+        }
+        var jointIndex = Object.keys(jointsAndWeights)[i]
+        vertexJointAffectors[vertexPositionIndex * 4 + i] = Number(jointIndex) || 0
+        vertexJointWeights[vertexPositionIndex * 4 + i] = jointsAndWeights[jointIndex] || 0
+      }
+      encounteredIndices[vertexPositionIndex] = true
+    }
+  })
+  parsedDae.vertexPositionIndices.forEach(function (vertexPositionIndex, counter) {
+    if (encounteredIndices[vertexPositionIndex]) {
+      vertexPositionIndices[counter] = ++largestPositionIndex
+      var jointsAndWeights = parsedDae.vertexJointWeights[vertexPositionIndex]
+      for (var i = 0; i < 4; i++) {
+        if (i < 3) {
+          parsedDae.vertexPositions[largestPositionIndex * 3 + i] = parsedDae.vertexPositions[vertexPositionIndex * 3 + i]
+          vertexNormals[largestPositionIndex * 3 + i] = parsedDae.vertexNormals[parsedDae.vertexNormalIndices[counter] * 3 + i]
+        }
+        var jointIndex = Object.keys(jointsAndWeights)[i]
+        vertexJointAffectors[largestPositionIndex * 4 + i] = Number(jointIndex) || 0
+        vertexJointWeights[largestPositionIndex * 4 + i] = jointsAndWeights[jointIndex] || 0
+      }
+    }
+  })
+
+  return {
+    vertexNormals: vertexNormals,
+    vertexPositionIndices: vertexPositionIndices,
+    vertexJointAffectors: vertexJointAffectors,
+    vertexJointWeights: vertexJointWeights
+  }
+}
--- demo/animated-model/3d-model/load-model.js
+++ demo/animated-model/3d-model/load-model.js
@@ -1,68 +1,15 @@
 var fs = require('fs')
 var dae2json = require('../../../')
 
+var expandVertices = require('./expand-vertices.js')
+
 module.exports = LoadModel
 
 // TODO: clean up
 function LoadModel (gl, callback) {
   // TODO: Read using xhr request
   var parsedDae = dae2json(fs.readFileSync('./demo/assets/animated-male-figure.dae'))
-
-  // Vertex normals
-  // TODO: Comment this
-  var vertexNormals = []
-
-  // TODO: naming
-  var vertexJointAffectors = []
-  var vertexJointWeights = []
-  /*
-     parsedDae.vertexJointWeights.forEach(function (jointsAndWeights) {
-     for (var i = 0; i < 4; i++) {
-     var jointIndex = Object.keys(jointsAndWeights)[i]
-     vertexJointAffectors.push(Number(jointIndex) || 0)
-     vertexJointWeights.push(jointsAndWeights[jointIndex] || 0)
-     }
-     })
-     */
-
-  // TODO: Can probably make this more efficient by indexing duplicate index -> normal -> new tail end index
-  // TODO: generally inefficient. Running calculations more than necessary. Still figuring out how to do this...
-  //  Throwing things in for now then optimize later
-  var encounteredIndices = {}
-  var vertexPositionIndices = []
-  var largestPositionIndex = 0
-  parsedDae.vertexPositionIndices.forEach(function (vertexPositionIndex, counter) {
-    largestPositionIndex = Math.max(largestPositionIndex, vertexPositionIndex)
-    if (!encounteredIndices[vertexPositionIndex]) {
-      var jointsAndWeights = parsedDae.vertexJointWeights[vertexPositionIndex]
-      // First time seeing the vertex position index
-      vertexPositionIndices[counter] = vertexPositionIndex
-      for (var i = 0; i < 4; i++) {
-        if (i < 3) {
-          vertexNormals[vertexPositionIndex * 3 + i] = parsedDae.vertexNormals[parsedDae.vertexNormalIndices[counter] * 3 + i]
-        }
-        var jointIndex = Object.keys(jointsAndWeights)[i]
-        vertexJointAffectors[vertexPositionIndex * 4 + i] = Number(jointIndex) || 0
-        vertexJointWeights[vertexPositionIndex * 4 + i] = jointsAndWeights[jointIndex] || 0
-      }
-      encounteredIndices[vertexPositionIndex] = true
-    }
-  })
-  parsedDae.vertexPositionIndices.forEach(function (vertexPositionIndex, counter) {
-    if (encounteredIndices[vertexPositionIndex]) {
-      vertexPositionIndices[counter] = ++largestPositionIndex
-      var jointsAndWeights = parsedDae.vertexJointWeights[vertexPositionIndex]
-      for (var i = 0; i < 4; i++) {
-        if (i < 3) {
-          parsedDae.vertexPositions[largestPositionIndex * 3 + i] = parsedDae.vertexPositions[vertexPositionIndex * 3 + i]
-          vertexNormals[largestPositionIndex * 3 + i] = parsedDae.vertexNormals[parsedDae.vertexNormalIndices[counter] * 3 + i]
-        }
-        var jointIndex = Object.keys(jointsAndWeights)[i]
-        vertexJointAffectors[largestPositionIndex * 4 + i] = Number(jointIndex) || 0
-        vertexJointWeights[largestPositionIndex * 4 + i] = jointsAndWeights[jointIndex] || 0
-      }
-    }
-  })
+  var vertexData = expandVertices(parsedDae)
 
   var vertexPositionBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer)
@@ -70,23 +17,23 @@ function LoadModel (gl, callback) {
 
   var vertexPositionIndexBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexPositionIndexBuffer)
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexPositionIndices), gl.STATIC_DRAW)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexData.vertexPositionIndices), gl.STATIC_DRAW)
 
   // Joints the affect each vertex
   // TODO: naming
   var affectingJointIndexBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, affectingJointIndexBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexJointAffectors), gl.STATIC_DRAW)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData.vertexJointAffectors), gl.STATIC_DRAW)
 
   // Weights of affecting joints
   // TODO: naming
   var weightBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, weightBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexJointWeights), gl.STATIC_DRAW)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData.vertexJointWeights), gl.STATIC_DRAW)
 
   var vertexNormalBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData.vertexNormals), gl.STATIC_DRAW)
 
   var stuff = {
     vertexNormalBuffer: vertexNormalBuffer,
@@ -95,7 +42,7 @@ function LoadModel (gl, callback) {
     affectingJointIndexBuffer: affectingJointIndexBuffer,
     weightBuffer: weightBuffer,
     keyframes: parsedDae.keyframes,
-    numElements: vertexPositionIndices.length
+    numElements: vertexData.vertexPositionIndices.length
   }
   callback(null, stuff)
 }
