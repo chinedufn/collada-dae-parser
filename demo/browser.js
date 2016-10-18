27acd3c34f11dbedd3b1274132a511006567e2a2
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-10-18 16:26:58 -0400

    Add fork me on GitHub

--- demo/browser.js
+++ demo/browser.js
@@ -8,15 +8,24 @@ incompleteWarning.style.backgroundColor = 'white'
 incompleteWarning.style.width = '90%'
 incompleteWarning.style.margin = '0 auto'
 
-var sourceLink = document.createElement('a')
-sourceLink.href = 'https://github.com/chinedufn/collada-dae-parser'
-sourceLink.innerHTML = 'View source on GitHub'
+// Add fork me on GitHub ribbon
+var forkMeOnGitHub = document.createElement('a')
+forkMeOnGitHub.href = 'https://github.com/chinedufn/collada-dae-parser'
+var ribbonImage = document.createElement('img')
+ribbonImage.style.position = 'absolute'
+ribbonImage.style.top = 0;
+ribbonImage.style.right = 0;
+ribbonImage.style.border = 0;
+ribbonImage.src = 'https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67'
+ribbonImage.alt = 'Fork me on GitHub'
+// <a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
+forkMeOnGitHub.appendChild(ribbonImage)
 
 incompleteWarning.innerHTML = 'Work in progress. - Parse collada .dae files for viewing 3d model animations in the browser. - Rotate camera using arrow keys. - '
-incompleteWarning.appendChild(sourceLink)
 
 document.body.appendChild(incompleteWarning)
 document.body.appendChild(mount)
+document.body.appendChild(forkMeOnGitHub)
 
 document.querySelector('html').style.height = '100%'
 document.body.style.height = '100%'
