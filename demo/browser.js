ceb3cb4239c3deff038ae3875927123be1bb7d2f
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-10-18 19:38:55 -0400

    Fix lint

--- demo/browser.js
+++ demo/browser.js
@@ -13,9 +13,9 @@ var forkMeOnGitHub = document.createElement('a')
 forkMeOnGitHub.href = 'https://github.com/chinedufn/collada-dae-parser'
 var ribbonImage = document.createElement('img')
 ribbonImage.style.position = 'absolute'
-ribbonImage.style.top = 0;
-ribbonImage.style.right = 0;
-ribbonImage.style.border = 0;
+ribbonImage.style.top = 0
+ribbonImage.style.right = 0
+ribbonImage.style.border = 0
 ribbonImage.src = 'https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67'
 ribbonImage.alt = 'Fork me on GitHub'
 // <a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
